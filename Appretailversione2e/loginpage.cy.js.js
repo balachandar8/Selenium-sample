a5af102670acaa54d703dc1d4f295790ba5782b5
balachandar8 <balachandar1998try@gmail.com>
2023-12-06 08:27:51 +0530

    Appretailversione2e file added

---
+++ Appretailversione2e/loginpage.cy.js.js
@@ -0,0 +1,12 @@
+describe("login page",() =>{
+    const lp = new loginpage()
+
+    cy.visit('https://testv2.appretail.io/login')
+    
+
+    // appretaillink({
+
+    //     cy.()
+    // })
+
+})
